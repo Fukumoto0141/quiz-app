2b6883fd9d3bc65f5440b9531463afcbbbbde54d
わけわかめ <>
2023-08-18 12:17:29 +0900

    ・回答フォーム実装 ・回答送信ボタン実装

--- angular-app/src/app/pages/main-game-screen/main-game-screen.component.ts
+++ angular-app/src/app/pages/main-game-screen/main-game-screen.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, doc, docData, onSnapshot } from '@angular/fire/firestore';
+import { Observable, take } from 'rxjs';
+import { hp } from 'src/app/question';
+import { QuizService } from 'src/app/service/quiz.service';
 
 @Component({
   selector: 'app-main-game-screen',
   templateUrl: './main-game-screen.component.html',
   styleUrls: ['./main-game-screen.component.scss']
 })
+
 export class MainGameScreenComponent {
-  value:number = 0;
-  add(x:number){
-  this.value = x;
-  }
+
+  private firestore: Firestore = inject(Firestore);
+
+  value: number = 0;
+  //DBを監視して更新があれば反映させる
+  unsub = onSnapshot(doc(this.firestore, "hp", "IUoZMfJV98TgINSdtigE"), (doc) => {
+    this.value = doc.get('hp');
+    console.log("Current data: ", doc.get('hp'));
+})
+
 }
